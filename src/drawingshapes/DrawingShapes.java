4ad95e6ac18ebb2214dc7629ba62ef265d2cdb52
JCBoyd <jcboyd@pm.me>
2020-03-03 20:14:13 -0700

    Basic program complete (triangle & rectangle with hard coded values

--- src/drawingshapes/DrawingShapes.java
+++ src/drawingshapes/DrawingShapes.java
@@ -9,6 +9,8 @@ package drawingshapes;
  * This program draws shapes using characters based on user input
  *****************************************************************************/
 
+import java.util.Scanner;
+
 public class DrawingShapes {
 
     public static void drawBar(int length)
@@ -20,11 +22,31 @@ public class DrawingShapes {
         System.out.println();
     } //end drawBar
     
+    public static void drawBox(int length, int height)
+    {
+        for (int i = 1; i <= height; i++)
+        {
+            drawBar(length);
+        } //end for
+    } //end drawBox
     
     
-    
-    public static void main(String[] args) {
-
+    public static void main(String[] args) 
+    {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("Drawing Project by JC Boyd\n");
+        
+        //draw a right triangle
+        for (int i = 1; i <= 6; i++)
+        {
+            drawBar(i);
+        } //end for
+        System.out.println();
+        
+        //draw a box
+        drawBox(5, 4);
+        System.out.println();
+        
     } //end main
     
 } //end class DrawingShapes
