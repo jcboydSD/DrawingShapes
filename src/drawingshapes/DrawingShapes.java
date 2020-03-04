60a02f0e7aebd97f595950e74a396b5b43cb692d
JCBoyd <jcboyd@pm.me>
2020-03-03 21:08:03 -0700

    Added comments for documentation

--- src/drawingshapes/DrawingShapes.java
+++ src/drawingshapes/DrawingShapes.java
@@ -3,7 +3,7 @@ package drawingshapes;
 /******************************************************************************
  * DrawingShapes.java 
  * Programmer: @author jcboyd
- * Version: 1.0
+ * Version: 2.0
  * Course: SDEV 2210
  *
  * This program draws shapes using characters based on user input
@@ -13,8 +13,8 @@ import java.util.Scanner;
 
 public class DrawingShapes {
 
-    public static void drawBar(int length)
-    {
+    public static void drawBar(int length) 
+    { //single line, default marker
         for (int i = 1; i <= length; i++)
         {
             System.out.print("*");
@@ -22,8 +22,8 @@ public class DrawingShapes {
         System.out.println();
     } //end drawBar
 
-    public static void drawBar(int length, String marker)
-    {
+    public static void drawBar(int length, String marker) 
+    { //single line, custom marker
         for (int i = 1; i <= length; i++)
         {
             System.out.print(marker);
@@ -31,8 +31,8 @@ public class DrawingShapes {
         System.out.println();
     } //end drawBar
     
-    public static void drawBox(int length, int height)
-    {
+    public static void drawBox(int length, int height)  
+    { //multiple lines, default marker                                                 
         for (int i = 1; i <= height; i++)
         {
             drawBar(length);
@@ -40,7 +40,7 @@ public class DrawingShapes {
     } //end drawBox
     
     public static void drawBox(int length, int height, String marker)
-    {
+    { //multiple lines, custom marker
         for (int i = 1; i <= height; i++)
         {
             drawBar(length, marker);
@@ -50,42 +50,40 @@ public class DrawingShapes {
     public static void main(String[] args) 
     {
         Scanner stdIn = new Scanner(System.in);
-        System.out.println("Drawing Project by JC Boyd\n");
+        int width, height; //line and box dimensions
+        String drawAnswer; //controls repeat loop
+        String markerAnswer; //determines overloaded method 
+        String marker;
         
-        //draw a right triangle
-        for (int i = 1; i <= 6; i++)
+        System.out.println("Drawing Project by JC Boyd");   
+        
+        do 
         {
-            drawBar(i);
-        } //end for
-        System.out.println();
-        
-        //draw a box
-        drawBox(5, 4);
-        System.out.println();
-        
-        //user input for box
-        int width, height;
-        String answer;
-        String marker = "*";
-        
-        System.out.print("How wide is the box? ");
-        width = stdIn.nextInt();
-        System.out.print("How high is the box? ");
-        height = stdIn.nextInt();
-        stdIn.nextLine();
-        System.out.print("Do you want the default marker (asterisk), Y or N? ");
-        answer = stdIn.nextLine();
-        if (answer.matches("n|N"))
-        {
-            System.out.print("What character do you want to use? ");
-            marker = stdIn.nextLine();
-            drawBox(width, height, marker);          
-        }
-        else
-        { 
-            drawBox(width, height);
-        } //end if
-        
+            System.out.print("\nDo you want me to draw a box? (Y or N) ");
+            drawAnswer = stdIn.nextLine();
+      
+            if (drawAnswer.equalsIgnoreCase("y"))
+            {
+                System.out.print("How wide is the box? ");
+                width = stdIn.nextInt();
+                System.out.print("How high is the box? ");
+                height = stdIn.nextInt();
+                stdIn.nextLine(); //clear Scanner input
+                System.out.print("Do you want the default marker (asterisk), Y or N? ");
+                markerAnswer = stdIn.nextLine();
+                if (markerAnswer.equalsIgnoreCase("n"))
+                {
+                    System.out.print("What character do you want to use? ");
+                    marker = stdIn.nextLine();
+                    drawBox(width, height, marker); //call custom marker method
+                }
+                else
+                { 
+                    drawBox(width, height); //call default marker method
+                } //end if  
+            } //end if
+        } while (drawAnswer.equalsIgnoreCase("y"));
+          
     } //end main
     
 } //end class DrawingShapes
